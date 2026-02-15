9924c7927c0c27f844a1a17873229c50e9c89bee
Nicholas Skelton <nicholasskelton30@gmail.com>
2026-02-15 12:59:25 -0600

    Week 5 Push

--- week-4/in-n-out-books/test/app.spec.js
+++ week-4/in-n-out-books/test/app.spec.js
@@ -34,3 +34,33 @@ describe("Chapter 3: API Tests", () => {
     expect(res.body.message).toEqual("Input must be a number");
   });
 });
+
+describe ("Chapter 4: API Tests", () => {
+  it("Returns 201 status code when adding a new book", async () => {
+
+    const res = await request(app).post("/api/books").send({
+      id: 6,
+      title: "Jurassic Park",
+      author: "Michael Crichton",
+    });
+
+    expect(res.statusCode).toEqual(201);
+  });
+
+  it("Returns 400 status code when adding a book without a title", async () => {
+
+    const res = await request(app).post("/api/books").send({
+      id: 100,
+      author: "Michael Crichton"
+    });
+
+      expect(res.statusCode).toEqual(400);
+      expect(res.body.message).toEqual("Bad Request");
+  });
+
+  it("Returns 204 status code when deleting a book", async () => {
+    const res = await request(app).delete("/api/books/3");
+    expect(res.statusCode).toEqual(204);
+  });
+
+});
